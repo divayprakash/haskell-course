1a269feb761e2e87a591ff204a21fb2049caa52f
Divay Prakash <divayprakash3@gmail.com>
2017-02-23 20:02:05 +0530

    Part 2a in progress

--- assignment2.hs
+++ assignment2.hs
@@ -4,14 +4,16 @@ count = map length
 equal :: [Int] -> Bool
 equal xs = and $ map (== head xs) (tail xs)
 
-is_matrix :: [[a]] -> Bool
-is_matrix x
-	| x == [] = False
-	| otherwise = func x (count x)
-	where
-		func x (count x)
-			
+zero_exists :: [Int] -> Bool
+zero_exists = all (==0)
 
+is_matrix :: [[a]] -> Bool
+is_matrix [] = False
+is_matrix x = func x (count x)
+	where
+		func x y
+			| 0 `elem` y = False
+			| otherwise = equal (count x)
 
 --is_matrix [[],[],[]] = False
 --is_matrix [[2,3], [4,5], [6,7]] = True
