c84267110065930d2f74678d4d1793be09b99963
Divay Prakash <divayprakash3@gmail.com>
2017-02-23 20:11:01 +0530

    Swap spaces for tabs

--- assignment2.hs
+++ assignment2.hs
@@ -6,8 +6,8 @@ equal xs = and $ map (== head xs) (tail xs)
 
 is_matrix_helper :: [[a]] -> [Int] -> Bool
 is_matrix_helper x y
-	| 0 `elem` y = False
-	| otherwise = equal (count x)
+    | 0 `elem` y = False
+    | otherwise = equal (count x)
 
 is_matrix :: [[a]] -> Bool
 is_matrix [] = False
