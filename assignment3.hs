94e6c838f1b13d500e0e6337e2b66da5b947675d
Divay Prakash <divayprakash3@gmail.com>
2017-03-16 18:19:49 +0530

    Part 3 done

--- assignment3.hs
+++ assignment3.hs
@@ -17,9 +17,39 @@ nohundred n
     | n <= 0    = 0
     | otherwise = nohundred_list !! n
 
-infList :: [Integer]
+merge :: [Int] -> [Int] -> [Int]
+merge [] ys = ys
+merge xs [] = xs
+merge (x:xs) (y:ys)
+    | x <= y    = x:(merge xs (y:ys))
+    | otherwise = y:(merge (x:xs) ys)
+
+mergesort :: [Int] -> [Int]
+mergesort []  = []
+mergesort [x] = [x]
+mergesort l   = merge (mergesort (front l)) (mergesort (back l))
+    where
+        front l = take ((length l) `div` 2) l
+        back l  = drop ((length l) `div` 2) l
+
+remDup::[Int]->[Int]
+remDup = remDupHelper []
+    where
+        remDupHelper seen [] = seen
+        remDupHelper seen (x:xs)
+            | x `elem` seen = remDupHelper seen xs
+            | otherwise     = remDupHelper (seen ++ [x]) xs
+
+infListCreate :: Int -> Int -> [Int] -> [Int]
+infListCreate n i list
+ | n > i = (infListCreate n (i + 1) (mergesort (remDup (((list !! i) * 2) : ((list !! i) * 3) : ((list !!i) * 5) : list))))
+ | otherwise = list
 
 infListElem :: Int -> Int
 infListElem n
-    | n < 0     = 0
-    | otherwise = infList !! (n - 1)
+    | n <= 0    = 0
+    | n == 1    = 1
+    | otherwise = (infListCreate n 0 [1]) !! (n - 1)
+
+infList :: [Integer]
+infList = [toInteger (infListElem i) | i <- [1..]]
