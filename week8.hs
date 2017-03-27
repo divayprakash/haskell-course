7a56a232ff7eba9572bc54744a51a0a716ee443b
Divay Prakash <divayprakash3@gmail.com>
2017-03-28 03:30:25 +0530

    Fixed for re-submission

--- week8.hs
+++ week8.hs
@@ -1,10 +1,24 @@
+import System.IO
 import System.Exit
 
-main :: IO ()
 main = do
     str <- getLine
     if (str /= "EOF")
         then do
-            putStrLn (reverse str)
+            appendFile "temp" (str ++ "\n")
             main
-        else exitSuccess
+        else do
+            withFile "temp" ReadWriteMode (\handle -> do
+                contents <- hGetContents handle
+                let ls = lines contents
+                printfunc ls (length ls))
+
+printfunc :: [String] -> Int -> IO ()
+printfunc lines length
+    | length > 0  = printfunchelper lines length
+    | length == 0 = exitSuccess
+
+printfunchelper lines length = do
+    writeFile "temp" ""
+    putStrLn (reverse (lines!!(length - 1)))
+    printfunc lines (length - 1)
